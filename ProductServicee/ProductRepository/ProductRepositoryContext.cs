0b058cef6ac816417cba8f07d10740b85880e204
Kirill Vusov <kvusov@bk.ru>
2025-05-13 18:03:24 +0300

    Added repository, repository context and configuration

---
+++ ProductServicee/ProductRepository/ProductRepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+using Repository.Configurations;
+using System.Reflection.Emit;
+namespace Repository
+{
+    public class ProductRepositoryContext : DbContext
+    {
+        public ProductRepositoryContext(DbContextOptions opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
