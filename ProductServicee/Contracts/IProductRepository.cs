1483bd65b045c16b569c9f617ce8aa40bcde613e
Kirill Vusov <kvusov@bk.ru>
2025-05-13 17:58:00 +0300

    Created contects and entities.

---
+++ ProductServicee/Contracts/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using System.Linq.Expressions;
+
+namespace Contracts
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> FindAll(bool trackChanges);
+        IQueryable<Product> FindByCondition(Expression<Func<Product, bool>> expression, bool trackChanges);
+        void CreateProduct(Product product);
+        void DeleteProduct(Product product);
+        void UpdateProduct(Product product);
+        Task SaveProductAsync();
+    }
+}
