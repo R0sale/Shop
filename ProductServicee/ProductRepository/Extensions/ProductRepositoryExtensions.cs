53060e14dbcb7a3fa80522f92c36974a69d584ed
Kirill Vusov <kvusov@bk.ru>
2025-05-14 18:06:51 +0300

    Added filtering

---
+++ ProductServicee/ProductRepository/Extensions/ProductRepositoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions
+{
+    public static class ProductRepositoryExtensions
+    {
+        public static IQueryable<Product> FilterProduct(this IQueryable<Product> products, decimal minPrice, decimal maxPrice, DateTime minCreationDate, DateTime maxCreationDate)
+        {
+            var filteredProduct = products.Where(p => p.Price < maxPrice && 
+            p.Price > minPrice && 
+            p.CreationDate < maxCreationDate && 
+            p.CreationDate > minCreationDate);
+
+            return filteredProduct;
+        }
+    }
+}
